fe36f104fb42297fbddd96cc795c2f7df2856bf7
sonhalong97 <165064150+sonhalong97@users.noreply.github.com>
2024-03-27 14:38:09 +0700

    install.sh

--- proxyv4/bash/install.sh
+++ proxyv4/bash/install.sh
@@ -4,6 +4,10 @@ install_squid() {
     yum -y install squid
 }
 
+install_httpd_tools() {
+    yum -y install httpd-tools
+}
+
 backup_squid_config() {
     cp /etc/squid/squid.conf /etc/squid/squid.conf.bak
 }
@@ -12,20 +16,22 @@ port() {
     echo $(shuf -i 2000-65000 -n 1)
 }
 
-random() {
+random_password() {
     cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1
 }
 
 configure_squid() {
     local port=$(port)
-    local username=$(random)
-    local password=$(random)
+    local username=$(random_password)
+    local password=$(random_password)
 
     sed -i "s/http_port 3128/http_port $port/" /etc/squid/squid.conf
     sed -i "s/http_access deny all/http_access allow all/" /etc/squid/squid.conf
     sed -i "/# INSERT_AUTHENTICATION_RULE_HERE/i auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/passwd\nauth_param basic realm proxy\nacl authenticated proxy_auth REQUIRED\nhttp_access allow authenticated\n" /etc/squid/squid.conf
 
     htpasswd -bc /etc/squid/passwd $username $password
+    
+    echo "$username:$password" > /etc/squid/stpasswd
     
     port=$(grep -oP 'http_port \K\d+' /etc/squid/squid.conf)
 }
@@ -54,6 +60,7 @@ send_to_google_sheet() {
 
 main() {
     install_squid
+    install_httpd_tools
     backup_squid_config
     configure_squid
     restart_squid
